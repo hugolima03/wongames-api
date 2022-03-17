76c08009fe7f67461351d4089959f12102159e8f
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-17 15:39:10 -0400

    init populate route and controller

--- src/api/game/controllers/game.js
+++ src/api/game/controllers/game.js
@@ -1,9 +1,18 @@
-'use strict';
+"use strict";
 
 /**
  *  game controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::game.game');
+module.exports = createCoreController("api::game.game", ({ strapi }) => ({
+  async populate(ctx) {
+    try {
+      console.log("Starting to populate...");
+      ctx.send("Finished populating!");
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+}));
--- src/api/game/routes/game.js
+++ src/api/game/routes/game.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  * game router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::game.game');
+// module.exports = createCoreRouter("api::game.game");
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/games",
+      handler: "game.find",
+    },
+    {
+      method: "POST",
+      path: "/games/populate",
+      handler: "game.populate",
+    },
+  ],
+};
